ebef0ef10b8db1acc24502dc4f94d06fd6b4a8c1
millene prado <millene_vprado@hotmail.com>
2022-02-11 23:41:42 -0300

    updated project filename

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const projects = [
-  '01 - JavaScript Drum Kit',
-  '02 - JS and CSS Clock',
+  '01-JavaScript-Drum-Kit',
+  '02-JS-and-CSS-Clock',
   '03 - CSS Variables',
   '04 - Array Cardio Day 1',
   '05 - Flex Panel Gallery',
