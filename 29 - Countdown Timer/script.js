5c1248d2b6965e4e7b34a71c44a20e75b6cad40d
Millene Prado <millene_vprado@hotmail.com>
2022-07-25 11:58:59 -0300

    finished displaying timer and endtime

---
+++ 29 - Countdown Timer/script.js
@@ -0,0 +1,46 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
+function timer(seconds) {
+  // when the timer started
+  const now = Date.now(); // in milliseconds
+  // when the timer ended
+  const then = now + seconds * 1000;
+  // to run the countdown immediately once the timer started
+  displayTimeLeft(seconds);
+  // to display the endtime
+  displayEndTime(then);
+
+  // after every single seconds, we need to display
+  // the amount of time left
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    // check if we should stop it
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  // to update the title of the HTML
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+// show the ending time
+function displayEndTime(timestamp) {
+  // timestamp is the 'then' and we want to convert it into a date
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
